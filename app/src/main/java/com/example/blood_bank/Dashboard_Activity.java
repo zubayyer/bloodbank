437c45fcf879509455cdfdc49209a4a2852f0cf8
zubayyer <mzubayyer1596@gmail.com>
2023-08-02 01:14:12 -0700

    aaa

--- app/src/main/java/com/example/blood_bank/Dashboard_Activity.java
+++ app/src/main/java/com/example/blood_bank/Dashboard_Activity.java
@@ -11,6 +11,5 @@ public class Dashboard_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        hosp =
     }
 }
