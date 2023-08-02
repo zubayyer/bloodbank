35cc4c61e0bd0c10b1351625dbd051be2b93e18b
zubayyer <mzubayyer1596@gmail.com>
2023-08-02 01:06:23 -0700

    aa

---
+++ app/src/main/java/com/example/blood_bank/Dashboard_Activity.java
@@ -0,0 +1,16 @@
+package com.example.blood_bank;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+
+public class Dashboard_Activity extends AppCompatActivity {
+    ConstraintLayout hosp,blood,donor,help;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        hosp =
+    }
+}
